e652b0f7756325ecec2db2f057c9beb474ab94da
fianbastianm-source <fianbastianm@gmail.com>
2026-07-15 21:41:29 +1000

    Restructure: move project to repo root

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main className="p-10">
+      <h1 className="text-4xl font-bold">Hi, I'm Fian</h1>
+      <p className="text-gray-600 mt-2">Full-stack developer building things.</p>
+    </main>
+  );
+}
